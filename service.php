1d3c2754edd67bb6f17580c80764d1d28b3202d2
The End 7moz <k8668975@gmail.com>
2018-08-04 16:45:34 +0300

    First commit

---
+++ service.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>CANNY | Service</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <?php include "style.php" ?>
+</head>
+
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header Container
+    ================================================== -->
+    <?php include "header.php"; ?>
+    <div class="clearfix"></div>
+    <!-- Header Container / End -->
+
+    <div id="titlebar" class="gradient margin-bottom-35">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h2>Service</h2>
+
+                    <!-- Breadcrumbs -->
+                    <nav id="breadcrumbs">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>Help</li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Banner
+    ================================================== -->
+    <section class="faq">
+        <div class="container">
+            <div class="service-container">
+                <div class="row mb-4">
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <h4 class="headline text-center text-uppercase">General Maintenance Sector</h4>
+                    </div>
+
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/01.png" alt="">
+                        <h5>
+                            Shower and TV
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/02.png" alt="">
+                        <h5>
+                            Home Appliances Maintenance
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/03.png" alt="">
+                        <h5>
+                            Cleaning furniture
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/04.png" alt="">
+                        <h5>
+                            Cooling and heating
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/05.png" alt="">
+                        <h5>
+                            Glass and aluminum
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/06.png" alt="">
+                        <h5>
+                            Ceramics and Marble
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/07.png" alt="">
+                        <h5>
+                            Electricity
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/08.png" alt="">
+                        <h5>
+                            Carpentry
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/09.png" alt="">
+                        <h5>
+                            Blacksmith
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/10.png" alt="">
+                        <h5>
+                            engraving
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/11.png" alt="">
+                        <h5>
+                            pest control
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/12.png" alt="">
+                        <h5>
+                            Garden Works
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/13.png" alt="">
+                        <h5>
+                            Cleaning houses
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/14.png" alt="">
+                        <h5>
+                            Renovations and decoration
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/15.png" alt="">
+                        <h5>
+                            Plumbing
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/16.png" alt="">
+                        <h5>
+                            Other maintenance
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="faq mt-4 pt-5" data-background-color="#f8f8f8">
+        <div class="container">
+            <div class="service-container">
+                <div class="row mb-4 ">
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <h4 class="headline text-center text-uppercase">Reservations</h4>
+                    </div>
+
+                </div>
+                <div class="row mt-3 justify-content-center" >
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/17.png" alt="">
+                        <h5>
+                            Restaurants
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/18.png" alt="">
+                        <h5>
+                            Clinics
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/19.png" alt="">
+                        <h5>
+                            Optics
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/20.png" alt="">
+                        <h5>
+                            Chalets
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/21.png" alt="">
+                        <h5>
+                            Wedding halls
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/22.png" alt="">
+                        <h5>
+                            Beauty salons
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/23.png" alt="">
+                        <h5>
+                            Kitchens
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/24.png" alt="">
+                        <h5>
+                            Clubs
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/25.png" alt="">
+                        <h5>
+                            Blacksmith
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/10.png" alt="">
+                        <h5>
+                            Places of entertainment
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="faq  pt-5">
+        <div class="container">
+            <div class="service-container">
+                <div class="row mb-4 ">
+                    <div class="col-md-8 offset-md-2 text-center">
+                        <h4 class="headline text-center text-uppercase">Delivery and transportation</h4>
+                    </div>
+
+                </div>
+                <div class="row mt-3 justify-content-center" >
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/26.png" alt="">
+                        <h5>
+                            Taxis Offices
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/27.png" alt="">
+                        <h5>
+                            Trucks
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/28.png" alt="">
+                        <h5>
+                            Tuk Tuk
+                        </h5>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <img src="images/service/29.png" alt="">
+                        <h5>
+                            Delverry
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Content
+    ================================================== -->
+
+    <!-- Footer
+    ================================================== -->
+    <?php include "footer.php" ?>
+    <!-- Footer / End -->
+
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+
+
+</div>
+<!-- Wrapper / End -->
+
+
+<!-- Scripts
+================================================== -->
+<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+<script type="text/javascript" src="scripts/mmenu.min.js"></script>
+<script type="text/javascript" src="scripts/chosen.min.js"></script>
+<script type="text/javascript" src="scripts/slick.min.js"></script>
+<script type="text/javascript" src="scripts/rangeslider.min.js"></script>
+<script type="text/javascript" src="scripts/magnific-popup.min.js"></script>
+<script type="text/javascript" src="scripts/waypoints.min.js"></script>
+<script type="text/javascript" src="scripts/counterup.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+<script type="text/javascript" src="scripts/tooltips.min.js"></script>
+<script type="text/javascript" src="scripts/custom.js"></script>
+
+
+</body>
+
+</html>
